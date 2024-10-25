c4573fb396179e3b54b8bc5d7eae3eae69883278
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2024-10-25 11:10:03 +0700

    Update imageSearch.ts

--- vocal-web/src/lib/imageSearch.ts
+++ vocal-web/src/lib/imageSearch.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const searchImages = (keyword) => {
-  if (!keyword) return []
+  if (!keyword) return new Promise(resolve => resolve([]))
 
   return axios.get(`https://api.pexels.com/v1/search?query=${keyword}&per_page=10`, {
     headers: {
