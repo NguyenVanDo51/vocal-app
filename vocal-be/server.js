3cc8507cf78fc7f97488564b850a373c7e8e2e5d
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2024-10-25 16:55:41 +0700

    Update server.js

--- vocal-be/server.js
+++ vocal-be/server.js
@@ -3,29 +3,21 @@ var cors = require('cors')
 const authRoutes = require('./modules/auth/authRoutes')
 const collectionRoutes = require('./modules/collection/collectionRoutes')
 const userRoutes = require('./modules/user/userRoutes')
+const authenticateJWT = require('./modules/auth/authMiddleware')
 
 const app = express()
 
-const corsOptions = {
-  origin: 'https://api.talk4learn.com', // Thay đổi thành miền cụ thể nếu cần
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Nếu bạn cần gửi cookies hoặc thông tin xác thực
-}
-
-app.use(cors(corsOptions))
-
 app.use(express.json())
+app.use(cors())
 
-app.use('/', () => {
-  return 'Hello World'
+app.get('/', (req, res) => {
+  res.json({ greeting: 'Hello world' })
 })
 
 app.use('/api', authRoutes)
 app.use('/api', collectionRoutes)
-app.use('/api', cors(), userRoutes)
+app.use('/api', userRoutes)
 
-const PORT = 3001
-
-app.listen(PORT, () => {
-  console.log('Server is running on port', PORT)
+app.listen(3333, () => {
+  console.log('Server is running on port 3333')
 })
