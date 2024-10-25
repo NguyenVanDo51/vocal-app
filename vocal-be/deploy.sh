122546b50675b50e2ee1b42a137e995c02b1fab4
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2024-10-25 11:51:10 +0700

    1

--- vocal-be/deploy.sh
+++ vocal-be/deploy.sh
@@ -2,4 +2,6 @@ git pull origin main
 
 pm2 stop vocabapi
 
+pm2 delete vocabapi
+
 npm run start:pm2
--- vocal-be/server.js
+++ vocal-be/server.js
@@ -5,21 +5,9 @@ const collectionRoutes = require('./modules/collection/collectionRoutes')
 const userRoutes = require('./modules/user/userRoutes')
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
-app.use(
-  cors({
-    origin: '*', // Cho phép tất cả các miền
-  })
-)
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  next()
-})
 
 app.use('/api', authRoutes)
 app.use('/api', collectionRoutes)
